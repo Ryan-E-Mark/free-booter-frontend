37dade3dabfd77bc2273db0dab7b899e0f776372
Ryan Mark <rymark907@gmail.com>
2021-11-29 18:10:33 -0800

    made navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import React from 'react';
+import Nav from './components/nav';
+// import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      
+      <Nav />
+      <h1>Freebooter</h1>
     </div>
   );
 }
---
+++ src/components/nav.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+function Nav() {
+    return (
+        <nav>
+            <div>
+                <h2>MTN SURF</h2>
+            </div>
+            <div>
+                <p>Soon to be links</p>
+            </div>
+        </nav>
+    )
+}
+
+export default Nav;
