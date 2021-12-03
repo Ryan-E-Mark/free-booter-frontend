676a5d37b7fe5bd806cc84be8a916f5c2b2d6cbd
Ryan Mark <rymark907@gmail.com>
2021-12-02 16:01:51 -0800

    got the logo img to work!

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,13 +1,13 @@
 import React from 'react';
-
+import './nav.css'
 
 function Nav() {
     return (
-        <nav>
-            <div>
-                <h2>MTN SURF</h2>
+        <nav className="nav-bar">
+            <div id="mtn-logo">
+                <img id="surf-logo" src="http://localhost:3000/static/media/mtn_srf.9aa238d1.png" alt="company logo"/>
             </div>
-            <ul>
+            <ul className="link-list">
                 <li><a href="#products">Products</a></li>
                 <li><a href="#about">About</a></li>
                 <li><a href="#contact">Contact</a></li>
