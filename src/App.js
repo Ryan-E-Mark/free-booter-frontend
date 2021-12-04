27ba1e399a6dc774f1127654262b13dacc53e056
Ryan Mark <rymark907@gmail.com>
2021-12-04 11:13:36 -0800

    trying to get video to play unsuccessfully

--- src/App.js
+++ src/App.js
@@ -9,9 +9,12 @@ function App() {
       <div className="App">
         <Nav />
         <h1>Freebooter</h1>
+        <h3>Mountain Surf</h3>
         <div>
           <p>Free your feet, free your soul</p>
         </div>   
+        <button>View our lineup</button>
+        <video src="../src/justinsurf.mp4" muted loop autoplay/>
       </div>
 
     </Router>
