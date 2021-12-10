5568b5688ce9e3ce1ed96cbd0b442412d416017d
Ryan Mark <rymark907@gmail.com>
2021-12-10 12:00:10 -0800

    added products component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes , Route } from 'react-router-dom';
 import Nav from './components/nav';
 import Home from './components/home';
+import Products from './components/products';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Nav />
       <Routes>
         <Route exact path="/" element={<Home/>} />
+        <Route path="/products" element={<Products/>} />
       </Routes>
     </Router>
   );
---
+++ src/components/products.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+
+
+function Products() {
+    
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Products;
