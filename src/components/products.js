f21d38ff816a153f24692e61f0fd41c3dacca433
Ryan Mark <rymark907@gmail.com>
2022-03-10 14:02:40 -0800

    background change. building out product page next :

--- src/components/products.js
+++ src/components/products.js
@@ -2,6 +2,41 @@ import React from 'react';
 import { fetchProducts } from '../actions/index';
 import { connect, useDispatch } from 'react-redux';
 
+const data = [
+    {
+    name: 'Slayer',
+    image: null,
+    size: null,
+    description: "the absolute best board ever",
+    type: 'board',
+    quantity: 1,
+},
+{
+    name: 'Slayer',
+    image: null,
+    size: null,
+    description: "the absolute best board ever",
+    type: 'board',
+    quantity: 1,
+},
+{
+    name: 'Slayer',
+    image: null,
+    size: null,
+    description: "the absolute best board ever",
+    type: 'board',
+    quantity: 1,
+},
+{
+    name: 'Slayer',
+    image: null,
+    size: null,
+    description: "the absolute best board ever",
+    type: 'board',
+    quantity: 1,
+},
+]
+
 
 function Products(props) {
     const dispatch = useDispatch()
@@ -20,6 +55,14 @@ function Products(props) {
         <div>
             <button onClick={handleClick}>CLICK ME</button>
             {props.product.status === "success" ? <img src={props.product.message} alt="random dog"></img> : <span></span>}
+            {data.map(prod => 
+                <div>
+                    <img src={prod.image} alt="product"></img>
+                    <h3>{prod.name}</h3>
+                    <p>{prod.size}</p>
+                    <p>{prod.description}</p>
+                </div>
+                )}
         </div>
     )
 }
