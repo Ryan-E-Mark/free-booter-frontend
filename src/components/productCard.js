47ed216d7c72369b734c0474d6d8cfbcccf03105
Ryan Mark <rymark907@gmail.com>
2022-04-13 14:18:59 -0700

    adjusting footer positioning

--- src/components/productCard.js
+++ src/components/productCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import boardimg from './imgs/Board.png';
 
 const ProductCard = ({product}) => {
 
@@ -8,7 +9,7 @@ const ProductCard = ({product}) => {
         <section className='product-section'>
             {product.map(prod => (
                 <div key={prod.product_id} className="prod-card">
-                    <img src={prod.img} alt="product thumbnail" className='prod-img'/>
+                    <img src={boardimg} alt="product thumbnail" className='prod-img'/>
                     <h4 className='prod-title'>{prod.product_name}</h4>
                     <p className='prod-size'>Size: {prod.product_size}</p>
                     <p className='prod-stock'>In stock: {prod.in_stock}</p>
